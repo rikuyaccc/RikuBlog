ba62419e010efa978e9c8c0fc3715333234b0b96
千葉陸矢 <chibarikuya@chibarikuyanoMacBook-Air.local>
2020-12-11 10:50:07 +0900

    postの投稿時間表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'bootstrap';
+import '../stylesheets/application';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +17,30 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+
+import '../stylesheets/application';
+
+document.addEventListener('turbolinks:load', () => {
+  document.addEventListener('click', () => {
+    let element = event.target.closest('.paragraph-content')
+    if (!element) return;
+
+    element.classList.add('d-none')
+    element.nextElementSibling.classList.remove('d-none')
+  })
+
+  document.addEventListener('click', () => {
+    if (!event.target.matches('.cancel')) return;
+
+    event.preventDefault();
+
+    let element = event.target.closest('.paragraph-form')
+
+    element.classList.add('d-none')
+    element.previousElementSibling.classList.remove('d-none')
+  })
+})
+import "controllers"
