808a5550fd8c14d01d27819d646e32114b5cf747
千葉陸矢 <chibarikuya@chibarikuyanoMacBook-Air.local>
2020-12-11 21:02:23 +0900

    postの新規投稿ページ修正

--- app/reflexes/elements_reflex.rb
+++ app/reflexes/elements_reflex.rb
@@ -1,10 +1,11 @@
 class ElementsReflex < ApplicationReflex
   def sort
-    # binding.pry
     elements = JSON.parse(element.dataset[:elements])
     elements.each do |element|
       element_record = Element.find(element['id'])
       element_record.update(position: element['position'])
     end
+    # NOTE: This is a hack! Might break with future versions of StimulusReflex!
+    @halted = true
   end
 end
